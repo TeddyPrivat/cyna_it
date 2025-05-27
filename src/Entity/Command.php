4736981004b12cb06a63ddc1a86df5282a67280f
ltrentesaux <leo.trentesaux53@gmail.com>
2025-05-27 20:54:41 +0200

    Panier et commande

--- src/Entity/Command.php
+++ src/Entity/Command.php
@@ -3,36 +3,37 @@
 namespace App\Entity;
 
 use App\Repository\CommandRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommandRepository::class)]
 class Command
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
     #[ORM\OneToOne(inversedBy: 'command', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
-    #[ORM\Column(type: Types::ARRAY, nullable: true)]
-    private ?array $product = null;
+    #[ORM\ManyToOne]
+    private ?Product $product = null;
 
-    #[ORM\Column(type: Types::ARRAY, nullable: true)]
-    private ?array $service = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?int $quantityProduct = null;
+    #[ORM\ManyToOne]
+    private ?Service $service = null;
 
     #[ORM\Column(nullable: true)]
-    private ?int $quantityService = null;
+    private ?int $quantity = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+        return $this;
     }
 
     public function getUser(): ?User
@@ -43,55 +44,39 @@ class Command
     public function setUser(User $user): static
     {
         $this->user = $user;
-
         return $this;
     }
 
-    public function getProduct(): ?array
+    public function getProduct(): ?Product
     {
         return $this->product;
     }
 
-    public function setProduct(?array $product): static
+    public function setProduct(?Product $product): static
     {
         $this->product = $product;
-
         return $this;
     }
 
-    public function getService(): ?array
+    public function getService(): ?Service
     {
         return $this->service;
     }
 
-    public function setService(?array $service): static
+    public function setService(?Service $service): static
     {
         $this->service = $service;
-
         return $this;
     }
 
-    public function getQuantityProduct(): ?int
+    public function getQuantity(): ?int
     {
-        return $this->quantityProduct;
+        return $this->quantity;
     }
 
-    public function setQuantityProduct(?int $quantityProduct): static
+    public function setQuantity(?int $quantity): static
     {
-        $this->quantityProduct = $quantityProduct;
-
-        return $this;
-    }
-
-    public function getQuantityService(): ?int
-    {
-        return $this->quantityService;
-    }
-
-    public function setQuantityService(?int $quantityService): static
-    {
-        $this->quantityService = $quantityService;
-
+        $this->quantity = $quantity;
         return $this;
     }
 }
