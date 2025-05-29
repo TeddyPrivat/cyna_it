b40ea78652d4a98888bf644f2294569ed860e5c4
TeddyPrivat <47928474+TeddyPrivat@users.noreply.github.com>
2025-05-30 01:17:38 +0200

    Possibilité de modifier un produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -52,10 +53,63 @@ final class ProductController extends AbstractController
         }
     }
 
-//    #[Route('/product/add', name: 'app_add_product', methods: ['POST'])]
-//    public function addProduct(ProductRepository $pr, EntityManagerInterface $em): JsonResponse{
-//
-////        $product = new Product();
-//
-//    }
+    #[Route('/product/add', name: 'app_add_product', methods: ['POST'])]
+    public function addProduct(EntityManagerInterface $em, Request $request): JsonResponse{
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return new JsonResponse(['error' => 'Invalid JSON'], 400);
+        }
+
+        $title = $data['title'];
+        $description = $data['description'];
+        $price = $data['price'];
+        $stock = $data['stock'];
+
+        if (!$title || !$description || $price === null || $stock === null) {
+            return new JsonResponse(['error' => 'Missing fields'], 400);
+        }
+        $product = new Product();
+        $product->setTitle($title);
+        $product->setDescription($description);
+        $product->setPrice($price);
+        $product->setStock($stock);
+        $product->setImgUrl("");
+
+        $em->persist($product);
+        $em->flush();
+
+        return new JsonResponse([
+            'message' => 'Produit ajouté avec succès',
+            'product' => [
+                'id' => $product->getId(),
+                'title' => $product->getTitle(),
+                'description' => $product->getDescription(),
+                'price' => $product->getPrice(),
+                'stock' => $product->getStock(),
+            ]
+        ], 201);
+    }
+
+    #[Route('/product/edit/{id}', name: 'app_edit_product', methods: ['PUT'])]
+    public function modifyProduct(int $id, ProductRepository $pr, EntityManagerInterface $em, Request $request): JsonResponse
+    {
+        $product = $pr->find($id);
+        if (!$product) {
+            return new JsonResponse(['error' => 'Produit non trouvé'], 404);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $product->setTitle($data['title'] ?? $product->getTitle());
+        $product->setDescription($data['description'] ?? $product->getDescription());
+        $product->setPrice($data['price'] ?? $product->getPrice());
+        $product->setStock($data['stock'] ?? $product->getStock());
+
+        $em->persist($product);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Produit mis à jour avec succès'], 200);
+
+    }
 }
