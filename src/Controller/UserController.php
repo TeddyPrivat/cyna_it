4e593497fd80945040926115a432778ce94110c4
WakakiVongola <133748743+WakakiVongola@users.noreply.github.com>
2025-06-07 19:51:20 +0200

    get user by id

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -26,4 +26,18 @@ final class UserController extends AbstractController
         }
         return $this->json($data);
     }
+//   get user by id
+    #[Route('/users/{id}', name: 'app_user', methods: ['GET'])]
+    public function getOneUser(UserRepository $ur, int $id): Response
+    {
+        $user = $ur->find($id);
+        $data = [
+            'id' => $user->getId(),
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+            'email' => $user->getEmail(),
+            'role' => $user->getRole()
+        ];
+        return $this->json($data);
+    }
 }
